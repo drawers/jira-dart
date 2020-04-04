86651a8d31c4fb8234f4b00bf591a81495ca8072
David Rawson <david.rawson@trademe.co.nz>
2020-04-05 11:58:30 +1200

    Add basic API prototype

---
+++ android/app/src/main/kotlin/com/tsongkha/jira/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tsongkha.jira
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:jira/jira/ticket.dart';
+import 'package:logger/logger.dart';
+
+import 'my_home_page.dart';
+
+void main() {
+  var logger = Logger();
+  var restClient = RestClient(Dio());
+  restClient.getTasks().then((a) => logger.e(a));
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
