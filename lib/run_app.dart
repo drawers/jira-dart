c59743251d3ff1505fe95f3b40e71418d4d7897e
David Rawson <david.rawson@trademe.co.nz>
2020-04-12 16:53:56 +1200

    Remove comment from docs

--- lib/run_app.dart
+++ lib/run_app.dart
@@ -8,9 +8,6 @@ import 'package:jira/main.dart';
 import 'jira/bloc/issues_event.dart';
 
 void runBlocLibraryApp(IssueRepository issueRepository) {
-  // BlocSupervisor oversees Blocs and delegates to BlocDelegate.
-  // We can set the BlocSupervisor's delegate to an instance of `SimpleBlocDelegate`.
-  // This will allow us to handle all transitions and errors in SimpleBlocDelegate.
   BlocSupervisor.delegate = LoggingBlocDelegate(logger);
   runApp(
     BlocProvider<IssuesBloc>(
