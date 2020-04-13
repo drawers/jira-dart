34a0161b7e10f51819e0cb15fde9e0bc578a22bf
David Rawson <david.rawson@trademe.co.nz>
2020-04-13 19:33:50 +1200

    Add bloc delegate inside common

--- lib/run_app.dart
+++ lib/run_app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jira/jira/bloc/issues_bloc.dart';
 import 'package:jira/jira/domain/repo/issue_repository.dart';
-import 'package:jira/logging_bloc_delegate.dart';
+import 'package:jira/jira/common/logging_bloc_delegate.dart';
 import 'package:jira/main.dart';
 
 import 'jira/bloc/issues_event.dart';
